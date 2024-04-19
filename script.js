e0342a943eecb721ae32a0d0f0b8f1420e9f65a1
Helloimalex1223 <plattaalex@gmail.com>
2024-04-19 10:24:11 -0900

    Add calculation to function that sets square size based on the size of the screen

--- script.js
+++ script.js
@@ -7,20 +7,22 @@ let sqNumber = document.querySelector(".numberSquares");
 sqNumber.addEventListener("click", () =>
 {
     let numInput = prompt("How many squares in a row/column do you want to create?");
-
     numberOfSquares(numInput);
-
 })
 
 
 //creates a certain number of squares based on the number the user enters
 function numberOfSquares(number)
 {
-    for(let i = 0; i < number; i++)
+    for(let i = 0; i < (number * number); i++)
     {
         //creates a square in the DOM
         let square = document.createElement("div");
         square.classList.add("sq");
+        
+        //calculates the height and width of a single square based on the screen height and width.
+        square.style.height =  "calc(600px / " + number + ")";
+        square.style.width =  "calc(600px / " + number + ")";
         sqWindow.appendChild(square);
     }
 }
