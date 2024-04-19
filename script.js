5818ae497d883023696d3f8a90004f2f475619f7
Helloimalex1223 <plattaalex@gmail.com>
2024-04-19 11:04:58 -0900

    Add skeleton function to change square color on click

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let sqWindow = document.querySelector(".screen");
 //Selects the button to get user input
 let sqNumber = document.querySelector(".numberSquares");
 
+let square;
+
 //gets user input to see how many squares to create
 sqNumber.addEventListener("click", () =>
 {
@@ -14,18 +16,29 @@ sqNumber.addEventListener("click", () =>
 //creates a certain number of squares based on the number the user enters
 function numberOfSquares(number)
 {
+    if(number > 100)
+    {
+        number = 100;
+    }
+
     //clear existing squares if any relevant. Don't want to overflow the screen element.
     clearSquares();
     for(let i = 0; i < (number * number); i++)
     {
         //creates a square in the DOM
-        let square = document.createElement("div");
+        square = document.createElement("div");
         square.classList.add("sq");
+
+        //add class to change square background color on mouseover
+        square.addEventListener("click", function(e) {
+            e.target.style.backgroundColor = "blue";
+        })
         
         //calculates the height and width of a single square based on the screen height and width.
         square.style.height =  "calc(600px / " + number + ")";
         square.style.width =  "calc(600px / " + number + ")";
         sqWindow.appendChild(square);
+        
     }
 }
 
@@ -44,4 +57,9 @@ function clearSquares()
     {
         sqWindow.removeChild(sqWindow.firstChild);
     };
+}
+
+function blueSq()
+{
+    square.style.backgroundColor = "blue";
 }
